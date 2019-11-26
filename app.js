985be6a81de918db23e39774e40e7145b77145f1
Federico Garcia <fedegarcia222@gmail.com>
2019-11-25 21:12:06 -0300

    a

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const rutasHeroes = require('./routes/heroes');
 const rutasMain = require('./routes/main');
 // Require de FS
-const fs = require('fs');
+
 
 // Ejecución de Express
 const app = express();
@@ -13,7 +13,7 @@ const app = express();
 app.listen(3030, () => console.log('Server running in 3030 port'));
 
 // Leyendo y parseando (en array) el contenido de heroes.json
-const heroes = JSON.parse(fs.readFileSync(__dirname + '/data/heroes.json', 'utf-8'));
+
 
 // Ruta Raíz / ➝ Home
 app.use('/',rutasMain);
@@ -22,28 +22,9 @@ app.use('/',rutasMain);
 app.use('/heroes',rutasHeroes);
 
 // Ruta /heroes/n ➝ se envía el nombre y profesión del héroe solicitado
-app.get('¿?', (¿?) => {
-	// Acá lo primero será encontrar al héroe que corresponda
-	let heroe = __¿?__;
-	
-	// Si se encuentra al héroe se envía el nombre y su profesión
-	// Si NO se encuentra se envía el mensaje de no encontrado
-});
 
-// Ruta /heroes/n/bio ➝ se envía la bio del héroe solicitado
-app.get('¿?', (¿?) => {
-	// Acá lo primero será encontrar al héroe que corresponda
-	let heroe = __¿?__;
 
-	// Si NO se encuentra al héroe se envía un mensaje
-	// Si se encuentra al héroe:
-		// Se pregunta si vino el parámetro Y el valor esperado y se envía la información
-		// Si nó vino el parámetro se envía el mensaje de error
-	}
-});
 
-// Ruta Créditos
-// ¿?
 
 // Ruta... ¿Pára qué sirve esto?
 app.get('*', (req, res) => {
